f1fd34d892d9fd1c606313cad7e5d5b382cdfe66
aborrelli20 <71987074+aborrelli20@users.noreply.github.com>
2022-02-03 10:24:10 -0500

    added function to POST request to get new task

--- application.js
+++ application.js
@@ -13,21 +13,28 @@ $(document).ready(function () {
     }
   });
 
-  $.ajax({
-    type: 'POST',
-    url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=230',
-    contentType: 'application/json',
-    dataType: 'json',
-    data: JSON.stringify({
-      task: {
-        content: 'Clean cat litter'
+  var createTask = function () {
+    $.ajax({
+      type: 'POST',
+      url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=230',
+      contentType: 'application/json',
+      dataType: 'json',
+      data: JSON.stringify({
+        task: {
+          content: $('#new-task-content').val()
+        }
+      }),
+      success: function (response, textStatus) {
+        console.log(response);
+      },
+      error: function (request, textStatus, errorMessage) {
+        console.log(errorMessage);
       }
-    }),
-    success: function (response, textStatus) {
-      console.log(response);
-    },
-    error: function (request, textStatus, errorMessage) {
-      console.log(errorMessage);
-    }
+    });
+  };
+
+  $('#create-task').on('submit', function (e) {
+    e.preventDefault();
+    createTask();
   });
 });
