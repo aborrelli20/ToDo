c308ade15e22125364edf948c3758f5973cdb5be
aborrelli20 <71987074+aborrelli20@users.noreply.github.com>
2022-02-03 10:16:11 -0500

    added loop to GET request

--- application.js
+++ application.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
-
-
   $.ajax({
     type: 'GET',
     url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=230',
     dataType: 'json',
     success: function (response, textStatus) {
-      console.log(response);
+      response.tasks.forEach(function (task) {
+        console.log(task.content);
+      })
     },
     error: function (request, textStatus, errorMessage) {
       console.log(errorMessage);
