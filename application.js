7a60f662156a3a7228f44243c23239e1afe930d9
aborrelli20 <71987074+aborrelli20@users.noreply.github.com>
2022-02-03 17:07:47 -0500

    added delete button and function

--- application.js
+++ application.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
       success: function (response, textStatus) {
         $('#todo-list').empty();
         response.tasks.forEach(function (task) {
-          $('#todo-list').append('<p>' + task.content + '</p>');
+          $('#todo-list').append('<div class="row"><p class="col-xs-8">' + task.content + '</p><button class="delete" data-id="' + task.id + '">Delete</button>');
         })
       },
       error: function (request, textStatus, errorMessage) {
@@ -44,14 +44,20 @@ $(document).ready(function () {
 
   getAndDisplayAllTasks();
 
-  $.ajax({
-    type: 'DELETE',
-    url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks/5729?api_key=230',
-    success: function (response, textStatus) {
-      console.log(response);
-    },
-    error: function (request, textStatus, errorMessage) {
-      console.log(errorMessage);
-    }
+  var deleteTask = function (id) {
+    $.ajax({
+      type: 'DELETE',
+      url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks/' + id + '?api_key=230',
+      success: function (response, textStatus) {
+        getAndDisplayAllTasks();
+      },
+      error: function (request, textStatus, errorMessage) {
+        console.log(errorMessage);
+      }
+    });
+  }
+
+  $(document).on('click', '.delete', function () {
+    deleteTask($(this).data('id'));
   });
 });
